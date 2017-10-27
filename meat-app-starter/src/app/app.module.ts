02e3463a420d15ee826e5d60514568b77ef09a43
Leonel Araujo <Leonel Araujo>
2017-10-26 23:54:24 -0300

    Refatoracao na tela de fechamento do pedido

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -21,6 +21,9 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
 import { InputComponent } from './shared/input/input.component';
+import { RadioComponent } from './shared/radio/radio.component';
+import { OrderItensComponent } from './order/order-itens/order-itens.component';
+import { OrderService } from './order/order.service';
 
 @NgModule({
   declarations: [
@@ -36,7 +39,9 @@ import { InputComponent } from './shared/input/input.component';
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent
+    InputComponent,
+    RadioComponent,
+    OrderItensComponent
   ],
   imports: [
     BrowserModule,
@@ -44,7 +49,7 @@ import { InputComponent } from './shared/input/input.component';
     FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [RestaurantsService, ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- meat-app-starter/src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ meat-app-starter/src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -3,28 +3,39 @@ import { CartItem } from './cart-item.model';
 
 export class ShoppingCartService {
 
-    items: CartItem[] = []
+    items: CartItem[] = [];
 
     clear() {
-        this.items = []
+        this.items = [];
     }
 
     addItem(item: MenuItem) {
         let foundItem = this.items.find((mItem) => mItem.menuItem.id === item.id)
         if (foundItem) {
-            foundItem.quantity = foundItem.quantity + 1
+            this.increaseQty(foundItem);
         } else {
-            this.items.push(new CartItem(item))
+            this.items.push(new CartItem(item));
+        }
+    }
+
+    increaseQty(item: CartItem) {
+        item.quantity = item.quantity + 1;
+    }
+
+    decreaseQty(item: CartItem) {
+        item.quantity = item.quantity - 1;
+        if (item.quantity === 0) {
+            this.removeItem(item);
         }
     }
 
     removeItem(item: CartItem) {
-        this.items.splice(this.items.indexOf(item), 1)
+        this.items.splice(this.items.indexOf(item), 1);
     }
 
     total(): number {
         return this.items
             .map(item => item.value())
-            .reduce((oldValue, newValue) => oldValue + newValue, 0) 
+            .reduce((oldValue, newValue) => oldValue + newValue, 0);
     }
 }
