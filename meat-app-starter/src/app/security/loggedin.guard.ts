3b821627961da5c2b6759d31e62c0e0f6e15c390
Leonel Araujo <leonel@gera3.com.br>
2017-12-20 08:46:06 -0300

    Implementação da interface canActiveted para validação de rotas

--- meat-app-starter/src/app/security/loggedin.guard.ts
+++ meat-app-starter/src/app/security/loggedin.guard.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
-import {CanLoad, Route} from '@angular/router'
+import { CanLoad, Route, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router'
 import { LoginService } from 'app/security/login/login.service';
 
 @Injectable()
-export class LoggedInGuard implements CanLoad {
+export class LoggedInGuard implements CanLoad, CanActivate {
 
     constructor(private loginService: LoginService) {}
 
+    checkAutentication(path: string): boolean {
+        const loggedIn = this.loginService.isLoggedIn()
+        if (!loggedIn) {
+            this.loginService.handleLogin(`/${path}`)
+        }
+        return loggedIn
+    }
+
     canLoad(route: Route): boolean {
-         const loggedIn = this.loginService.isLoggedIn()
-         if (!loggedIn) {
-            this.loginService.handleLogin(`/${route.path}`)
-         }
-         return loggedIn
+        return this.checkAutentication(route.path)
+    }
+
+    canActivate(activatedRoute: ActivatedRouteSnapshot, 
+                routerState: RouterStateSnapshot): boolean {
+        return this.checkAutentication(activatedRoute.routeConfig.path)
     }
 
 }
